9619228a1fc6955fd28c090b18b2b35def1f4291
Thomas O'Connor <114547491+thomaso6@users.noreply.github.com>
2022-11-12 21:57:29 -0600

    Update War_Test.js

--- War_Test.js
+++ War_Test.js
@@ -5,6 +5,6 @@ describe('functionTest', function(){
         it('should shuffle the array randomly and return a new array', function(){
             var x = shuffle([1, 2, 3, 4,]);
             expect(x).to.equal(this.cards.sort(() => Math.random() - 0.5)
-        })
-    })
-})
+        });
+    });
+});
